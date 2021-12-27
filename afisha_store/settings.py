4f1b54a9140ad6b5de644518bd87bf68fbe5ab59
domo <rtdokmo@gmail.com>
2021-12-27 21:51:02 +0300

    make it better

--- afisha_store/settings.py
+++ afisha_store/settings.py
@@ -150,3 +150,19 @@ LOGIN_URL = '/users/login/'
 AUTH_USER_MODEL = 'users.User'
 
 COUNT_PRODUCT_PAGE = 6
+
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
+
+import dj_database_url
+
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
